ecdfe832518544fd8194202729e6d7f4a6c5eb6f
alex <alex.mors@gmx.de>
2023-12-04 13:16:01 +0100

    day 4

---
+++ 2023/Python/src/day_04.py
@@ -0,0 +1,50 @@
+file = open("inputs/day_04.txt").readlines()
+
+
+def calc_points(number_of_wins: int) -> int:
+    if number_of_wins == 0:
+        return 0
+    points = 1
+    for _ in range(number_of_wins - 1):
+        points = points * 2
+    return points
+
+
+def part_1():
+    result = 0
+    for line in file:
+        left, right = line.strip().split(": ")[1].split(" | ")
+        won = {int(i) for i in left.split(" ") if i != ""}
+        played = {int(i) for i in right.split(" ") if i != ""}
+        number_of_wins = len(list(filter(lambda x: x in won, played)))
+        points = calc_points(number_of_wins)
+        result += points
+
+    print(f"Day 04, Part 1:", result)
+
+
+def part_2():
+    card_stack: dict[int, dict[str, int]] = {}
+
+    for line in file:
+        card_num = int(line.strip().split(": ")[0].split(" ")[-1])
+        left, right = line.strip().split(": ")[1].split(" | ")
+        won = {int(i) for i in left.split(" ") if i != ""}
+        played = {int(i) for i in right.split(" ") if i != ""}
+        number_of_wins = len(list(filter(lambda x: x in won, played)))
+
+        card_stack[card_num] = {"wins": number_of_wins, "count": 1}
+
+    for card_id, card in card_stack.items():
+        wins = card_stack[card_id]["wins"]
+        for _ in range(card["count"]):
+            for i in range(wins):
+                card_stack[card_id + i + 1]["count"] += 1
+
+    result = sum([v["count"] for v in card_stack.values()])
+    print(f"Day 04, Part 2:", result)
+
+
+if __name__ == "__main__":
+    part_1()
+    part_2()
