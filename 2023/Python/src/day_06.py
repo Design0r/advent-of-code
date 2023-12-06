05d95b43e42b47bc28574683757c1bd6a108b3be
alex <alex.mors@gmx.de>
2023-12-06 10:16:49 +0100

    day 6

---
+++ 2023/Python/src/day_06.py
@@ -0,0 +1,38 @@
+from functools import reduce
+from pathlib import Path
+
+file = open(Path(__file__).parent.parent / "inputs/day_06.txt").readlines()
+time = [int(i) for i in file[0].strip().split(":")[1].strip().split(" ") if i.isdigit()]
+distance = [
+    int(i) for i in file[1].strip().split(":")[1].strip().split(" ") if i.isdigit()
+]
+
+
+def day_1():
+    possible_wins = []
+    for t, d in zip(time, distance):
+        race = []
+        for i in range(t + 1):
+            if (t - i) * i > d:
+                race.append((t - i) * i)
+        possible_wins.append(len(race))
+
+    result = reduce(lambda x, y: x * y, possible_wins)
+    print("Day 06, Part 1:", result)
+
+
+def day_2():
+    possible_wins = 0
+    joined_time = int("".join(str(i) for i in time))
+    joined_distance = int("".join(str(i) for i in distance))
+
+    for i in range(joined_time + 1):
+        if (joined_time - i) * i > joined_distance:
+            possible_wins += 1
+
+    print("Day 06, Part 1:", possible_wins)
+
+
+if __name__ == "__main__":
+    day_1()
+    day_2()
