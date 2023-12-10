f84a75056bd4dcb31c2d7a4a525cfbc0d9b9ffdf
alex <alex.mors@gmx.de>
2023-12-10 20:49:39 +0100

    update day 8

--- 2023/Python/src/day_08.py
+++ 2023/Python/src/day_08.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import Generator
+import math
 
 file = open(Path(__file__).parent.parent / "inputs/day_08.txt").read()
 base_instruction = file.split("\n\n")[0].strip()
@@ -36,6 +37,16 @@ def day_1():
     print("Day 08, Part 1:", number_of_steps)
 
 
+def solve(nodes, current: str) -> int:
+    for idx, ins in enumerate(generate_instructions()):
+        if "Z" in current:
+            return idx
+        if ins == "L":
+            current = nodes[current][0]
+        elif ins == "R":
+            current = nodes[current][1]
+
+
 def day_2():
     nodes = {}
     for line in file.split("\n\n")[1].split("\n"):
@@ -44,10 +55,8 @@ def day_2():
         left, right = elements.replace("(", "").replace(")", "").split(", ")
         nodes[node] = (left, right)
 
-    number_of_steps = 0
     start = []
     end = set()
-    current = start
 
     for i in nodes.keys():
         if "A" in i:
@@ -55,25 +64,11 @@ def day_2():
         if "Z" in i:
             end.add(i)
 
-    instructions = generate_instructions()
-    while True:
-        instruction = next(instructions)
+    result = math.lcm(*[solve(nodes, s) for s in start])
 
-        for idx, i in enumerate(start):
-            if instruction == "L":
-                current[idx] = nodes[i][0]
-            elif instruction == "R":
-                current[idx] = nodes[i][1]
-
-        number_of_steps += 1
-        if set(current) == end:
-            break
-
-        if number_of_steps % 1_000_000 == 0:
-            print(number_of_steps)
-    print("Day 08, Part 2:", number_of_steps)
+    print("Day 08, Part 2:", result)
 
 
 if __name__ == "__main__":
-    # day_1()
+    day_1()
     day_2()
