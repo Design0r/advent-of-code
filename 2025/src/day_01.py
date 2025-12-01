15df2c459966ad666e1d31cd3307aad991fcafa4
alex <alex.mors@gmx.de>
2025-12-01 21:05:29 +0100

    2025 day 1 part 2

--- 2025/src/day_01.py
+++ 2025/src/day_01.py
@@ -34,6 +34,17 @@ def part_1(data: Data) -> None:
 @benchmark
 def part_2(data: Data) -> None:
     result = 0
+    curr_pos = 50
+
+    for line in data.lines:
+        sign = 1 if line[0] == "R" else -1
+        num = int(line[1:])
+
+        for _ in range(num):
+            curr_pos = (curr_pos + sign) % 100
+            if curr_pos == 0:
+                result += 1
+
     print(f"Day 01, Part 2: {result}")
 
 
